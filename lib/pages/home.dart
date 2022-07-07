3d57c8e525b4cf5a6f4bc0f28ca1d93eafd53425
takeru.abe <takeru.abe@gaiax.com>
2022-07-07 21:54:56 +0900

    create bottom_tab_index.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../constants/bottom_tab_index.dart';
+
 class Home extends ConsumerWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -14,23 +16,23 @@ class Home extends ConsumerWidget {
         child: Text('Center'),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
+        currentIndex: BottomTabIndex.home,
         selectedItemColor: Colors.orangeAccent,
         selectedLabelStyle: const TextStyle(color: Colors.orangeAccent),
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
               icon: Icon(Icons.home),
-              activeIcon: Icon(Icons.home, color: Colors.orangeAccent),
+              activeIcon: Icon(Icons.home),
               label: 'home',
               tooltip: 'ホーム画面'),
           BottomNavigationBarItem(
               icon: Icon(Icons.set_meal),
-              activeIcon: Icon(Icons.set_meal, color: Colors.orangeAccent),
+              activeIcon: Icon(Icons.set_meal),
               label: 'recipe',
               tooltip: 'レシピ'),
           BottomNavigationBarItem(
               icon: Icon(Icons.person),
-              activeIcon: Icon(Icons.person, color: Colors.orangeAccent),
+              activeIcon: Icon(Icons.person),
               label: 'mypage',
               tooltip: 'マイページ'),
         ],
---
+++ lib/providers/bottom_tab_index.dart
@@ -0,0 +1,7 @@
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../constants/bottom_tab_index.dart';
+
+// ignore_for_file: type=lint
+final bottomTabIndexProvider = StateProvider((ref) => BottomTabIndex.home);
