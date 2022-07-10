d71b681fc6a2967547d653ff26992f8de1d8b130
takeru.abe <takeru.abe@gaiax.com>
2022-07-10 10:21:55 +0900

    add http

--- lib/utils/google_vision/google_vision.dart
+++ lib/utils/google_vision/google_vision.dart
@@ -1,16 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:googleapis/vision/v1.dart';
-
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:http';
 
 
 class GoogleVision {
   GoogleVision() : assert(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS'] != null);
 
-  /// authenticate with JWT
+  /// authenticate with jwt
   static Future<GoogleVision> withJwt([String scope = 'https://www.googleapis.com/auth/cloud-vision']) async {
     GoogleVision vision = GoogleVision();
     String _json = await rootBundle.loadString(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS']!);
+    ServiceAccountCredentials accountCredentials = ServiceAccountCredentials.fromJson(jsonDecode(_json));
+    List<String> scopes = <String>[scope];
+
+    var client = http
 
     return vision;
   }
