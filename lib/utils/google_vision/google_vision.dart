d18a74b4b220aeceee020c9888253a925d390ddf
takeru.abe <takeru.abe@gaiax.com>
2022-07-10 11:14:03 +0900

    update google vision

--- lib/utils/google_vision/google_vision.dart
+++ lib/utils/google_vision/google_vision.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:googleapis/vision/v1.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,26 +10,24 @@ class GoogleVision {
   GoogleVision() : assert(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS'] != null);
 
   static AccessToken? _accessToken;
+  static AutoRefreshingAuthClient? _rest;
 
   String? get _token => _accessToken?.data;
   String? get _tokenType => _accessToken?.type;
   DateTime? get tokenExpiry => _accessToken?.expiry;
 
   /// authenticate with jwt
-  static Future<GoogleVision> withJwt(
-      [String scope = 'https://www.googleapis.com/auth/cloud-vision']) async {
+  static Future<GoogleVision> withJwt() async {
+    final GoogleVision googleVision = GoogleVision();
     String _json = await rootBundle
         .loadString(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS']!);
-    ServiceAccountCredentials accountCredentials =
-        ServiceAccountCredentials.fromJson(jsonDecode(_json));
-    List<String> scopes = <String>[scope];
+    ServiceAccountCredentials accountCredentials = ServiceAccountCredentials.fromJson(jsonDecode(_json));
+    List<String> scopes = <String>[VisionApi.cloudVisionScope];
+    AutoRefreshingAuthClient _client = await clientViaServiceAccount(
+        accountCredentials, scopes);
 
-    http.Client client = http.Client();
-    AccessCredentials credentials =
-        await obtainAccessCredentialsViaServiceAccount(
-            accountCredentials, scopes, client);
-    _accessToken = credentials.accessToken;
-    client.close();
-    return GoogleVision();
+    _accessToken = _client.credentials.accessToken;
+    _rest = _client;
+    return googleVision;
   }
 }
