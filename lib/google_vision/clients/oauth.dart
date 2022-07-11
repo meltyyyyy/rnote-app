4a3314c82d576d17ef05d6665b66bb986c67c5c9
takeru.abe <takeru.abe@gaiax.com>
2022-07-11 20:41:19 +0900

    create oauth.dart

---
+++ lib/google_vision/clients/oauth.dart
@@ -0,0 +1,17 @@
+
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart' hide Headers;
+
+import '../models/token.dart';
+
+part 'oauth.g.dart';
+
+@RestApi(baseUrl: 'https://accounts.google.com/o/oauth2')
+abstract class OAuthClient {
+  factory OAuthClient(Dio dio, {String baseUrl}) = _OAuthClient;
+
+  @POST('/token')
+  @Headers(
+      <String, dynamic>{'Content-Type': 'application/x-www-form-urlencoded'})
+  Future<Token> getToken(@Body() Map<String, dynamic> params);
+}
