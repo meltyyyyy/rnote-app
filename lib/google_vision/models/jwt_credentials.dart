91347946f156c6c3aee73b94dc8b65251c96d074
takeru.abe <takeru.abe@gaiax.com>
2022-07-11 20:36:59 +0900

    create jwt_credentials.dart

---
+++ lib/google_vision/models/jwt_credentials.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
+import 'json_settings.dart';
+
+part 'jwt_credentials.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class JwtCredentials {
+
+  JwtCredentials({required this.settings, required this.scope});
+
+  factory JwtCredentials.fromJson(Map<String, dynamic> json) =>
+      _$JwtCredentialsFromJson(json);
+  final JsonSettings settings;
+  final String scope;
+
+  Map<String, dynamic> toJson() => _$JwtCredentialsToJson(this);
+
+  @override
+  String toString() => jsonEncode(toJson());
+}
