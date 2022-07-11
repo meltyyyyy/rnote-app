5bee9d98e1226c637cc7771fa6f4a1a30275692e
takeru.abe <takeru.abe@gaiax.com>
2022-07-11 18:56:07 +0900

    create annotation_request.dart

---
+++ lib/google_vision/models/annotation_request.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:googleapis/vision/v1.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'annotation_request.g.dart';
+
+///Individual image annotation requests for this batch.
+@JsonSerializable(explicitToJson: true)
+class AnnotationRequest {
+
+  AnnotationRequest(
+      {required this.image, required this.features, this.imageContext});
+
+  factory AnnotationRequest.fromJson(Map<String, dynamic> json) =>
+      _$AnnotationRequestFromJson(json);
+  ///The image to be processed.
+  final Image image;
+
+  ///Requested features.
+  final List<Feature> features;
+
+  ///Additional context that may accompany the image.
+  final ImageContext? imageContext;
+
+  Map<String, dynamic> toJson() => _$AnnotationRequestToJson(this);
+
+  @override
+  String toString() => jsonEncode(toJson());
+}
