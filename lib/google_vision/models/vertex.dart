168b34e36cb1716dca1a375e830b905fb216e1be
takeru.abe <takeru.abe@gaiax.com>
2022-07-11 19:56:44 +0900

    create vertex.dart

---
+++ lib/google_vision/models/vertex.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'vertex.g.dart';
+
+///A vertex represents a 2D point in the image. NOTE: the vertex coordinates are
+///in the same scale as the original image.
+@JsonSerializable()
+class Vertex {
+
+  Vertex({required this.x, required this.y});
+
+  factory Vertex.fromJson(Map<String, dynamic> json) => _$VertexFromJson(json);
+  ///X coordinate.
+  @JsonKey(defaultValue: 0)
+  final int x;
+
+  ///Y coordinate.
+  @JsonKey(defaultValue: 0)
+  final int y;
+
+  Map<String, dynamic> toJson() => _$VertexToJson(this);
+
+  @override
+  String toString() => jsonEncode(toJson());
+}
