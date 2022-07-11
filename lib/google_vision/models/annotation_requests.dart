d7845bb4e8f25becf78736f942d7ec3d6751c953
takeru.abe <takeru.abe@gaiax.com>
2022-07-11 19:51:42 +0900

    create annotation_requests.dart

---
+++ lib/google_vision/models/annotation_requests.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
+import 'annotation_request.dart';
+
+part 'annotation_requests.g.dart';
+
+///Request for performing Google Cloud Vision API tasks over a user-provided
+///image, with user-requested features, and with context information.
+@JsonSerializable(explicitToJson: true)
+class AnnotationRequests {
+
+  AnnotationRequests({
+    required this.requests,
+  });
+
+  factory AnnotationRequests.fromJson(Map<String, dynamic> json) =>
+      _$AnnotationRequestsFromJson(json);
+  final List<AnnotationRequest> requests;
+
+  Map<String, dynamic> toJson() => _$AnnotationRequestsToJson(this);
+
+  @override
+  String toString() => jsonEncode(toJson());
+}
