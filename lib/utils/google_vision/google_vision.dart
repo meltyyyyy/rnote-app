6cd5f04696cdf178ba0f20d30fec5e7a10aa8bcd
takeru.abe <takeru.abe@gaiax.com>
2022-07-11 18:31:07 +0900

    add json_annotation

--- lib/utils/google_vision/google_vision.dart
+++ lib/utils/google_vision/google_vision.dart
@@ -1,54 +1,53 @@
-import 'dart:convert';
-
-import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:googleapis/vision/v1.dart';
-import 'package:googleapis_auth/auth_io.dart';
-import 'package:http/http.dart' as http;
-
-class GoogleVision {
-  GoogleVision() : assert(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS'] != null);
-
-  static AccessToken? _accessToken;
-  static AutoRefreshingAuthClient? _rest;
-
-  String? get _token => _accessToken?.data;
-  String? get _tokenType => _accessToken?.type;
-  DateTime? get tokenExpiry => _accessToken?.expiry;
-
-  /// authenticate with jwt
-  static Future<GoogleVision> withJwt() async {
-    final GoogleVision googleVision = GoogleVision();
-    String _json = await rootBundle
-        .loadString(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS']!);
-    ServiceAccountCredentials accountCredentials = ServiceAccountCredentials.fromJson(jsonDecode(_json));
-    List<String> scopes = <String>[VisionApi.cloudVisionScope];
-    AutoRefreshingAuthClient _client = await clientViaServiceAccount(
-        accountCredentials, scopes);
-
-    _accessToken = _client.credentials.accessToken;
-    _rest = _client;
-    return googleVision;
-  }
-
-  ///Run image detection and annotation for a batch of images.
-  Future<void> annotate(String image) async {
-    assert(_accessToken != null);
-    assert(_rest != null);
-
-    VisionApi _vision = VisionApi(_rest!);
-    ImagesResource _api = _vision.images;
-    BatchAnnotateImagesResponse response = await _api.annotate(BatchAnnotateImagesRequest.fromJson(<String, dynamic>{
-      'requests': [
-        {
-          'image': {'content': image},
-          'features': [
-            {'type': 'DOCUMENT_TEXT_DETECTION'}
-          ]
-        }
-      ]
-    }));
-    print(response);
-  }
-
-}
+// import 'dart:convert';
+//
+// import 'package:flutter/services.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:googleapis/vision/v1.dart';
+// import 'package:googleapis_auth/auth_io.dart';
+// import 'package:http/http.dart' as http;
+//
+// class GoogleVision {
+//   GoogleVision() : assert(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS'] != null);
+//
+//   static AccessToken? _accessToken;
+//   static AutoRefreshingAuthClient? _rest;
+//
+//   String? get _token => _accessToken?.data;
+//   String? get _tokenType => _accessToken?.type;
+//   DateTime? get tokenExpiry => _accessToken?.expiry;
+//
+//   /// authenticate with jwt
+//   static Future<GoogleVision> withJwt(String jwtPath) async {
+//     final GoogleVision googleVision = GoogleVision();
+//     String _json = await rootBundle.loadString(jwtPath);
+//     ServiceAccountCredentials accountCredentials =
+//         ServiceAccountCredentials.fromJson(jsonDecode(_json));
+//     List<String> scopes = <String>[VisionApi.cloudVisionScope];
+//     AutoRefreshingAuthClient _client =
+//         await clientViaServiceAccount(accountCredentials, scopes);
+//
+//     _accessToken = _client.credentials.accessToken;
+//     _rest = _client;
+//     return googleVision;
+//   }
+//
+//   ///Run image detection and annotation for a batch of images.
+//   Future<void> annotate(String image) async {
+//     assert(_accessToken != null);
+//     assert(_rest != null);
+//
+//     VisionApi _vision = VisionApi(_rest!);
+//     ImagesResource _api = _vision.images;
+//     BatchAnnotateImagesResponse response = await _api
+//         .annotate(BatchAnnotateImagesRequest.fromJson(<String, dynamic>{
+//       'requests': <Map<String, dynamic>>[
+//         <String, dynamic>{
+//           'image': <String, dynamic>{'content': image},
+//           'features': <Map<String, dynamic>>[
+//             <String, dynamic>{'type': 'DOCUMENT_TEXT_DETECTION'}
+//           ]
+//         }
+//       ]
+//     }));
+//   }
+// }
--- lib/utils/text_recognizer.dart
+++ lib/utils/text_recognizer.dart
@@ -1,1 +1,14 @@
-
+//
+// import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+//
+// import 'google_vision/google_vision.dart';
+//
+// class TextRecognizer {
+//   TextRecognizer() : assert(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS'] != null)
+//
+//   Future<void> recognize(Image image) async {
+//     final GoogleVision vision = await GoogleVision.withJwt(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS']!);
+//
+//   }
+// }
