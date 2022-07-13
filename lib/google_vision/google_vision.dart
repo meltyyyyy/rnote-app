c3435078958983ee3a02b4bd0a1b16bc25e05acb
takeru.abe <takeru.abe@gaiax.com>
2022-07-13 11:57:37 +0900

    temporal save

--- lib/google_vision/google_vision.dart
+++ lib/google_vision/google_vision.dart
@@ -1,165 +1,164 @@
+import 'dart:convert';
+
 import 'package:color/color.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
+import 'package:googleapis/vision/v1.dart';
+import 'package:googleapis_auth/auth_io.dart';
 
-import 'clients/vision.dart';
-import 'models/annotated_responses.dart';
-import 'models/annotation_requests.dart';
-import 'models/image.dart';
-import 'models/normalized_vertex.dart';
-import 'models/token.dart';
-import 'models/vertex.dart';
-import 'token_generator.dart';
+import 'models/annotation_request.dart';
+
+
 
 class GoogleVision {
-  GoogleVision() : _rest = VisionClient(dio);
-  final VisionClient _rest;
+  GoogleVision();
 
-  static final Dio dio = Dio();
-  static final DateTime tokenExpiry = DateTime(2010, 0, 0);
-  static const String accept = 'application/json';
-  static const String contentType = 'application/json';
+  static AccessToken? _accessToken;
+  static AutoRefreshingAuthClient? _rest;
 
-  static TokenGenerator? tokenGenerator;
-  static String? _token;
+  String? get _token => _accessToken?.data;
+  String? get _tokenType => _accessToken?.type;
+  DateTime? get tokenExpiry => _accessToken?.expiry;
 
-  String get _authHeader => 'Bearer $_token';
-
-  static Future<GoogleVision> withGenerator(TokenGenerator generator) async {
+  /// authenticate with jwt
+  static Future<GoogleVision> withJwt(String jwtPath) async {
     final GoogleVision googleVision = GoogleVision();
 
-    GoogleVision.tokenGenerator = generator;
+    String _json = await rootBundle.loadString(jwtPath);
+    ServiceAccountCredentials accountCredentials =
+        ServiceAccountCredentials.fromJson(jsonDecode(_json));
+    List<String> scopes = <String>[VisionApi.cloudVisionScope];
+    AutoRefreshingAuthClient _client =
+        await clientViaServiceAccount(accountCredentials, scopes);
 
-    await _confirmToken();
-
+    _accessToken = _client.credentials.accessToken;
+    _rest = _client;
     return googleVision;
   }
 
-  ///authenticated with JWT
-  static Future<GoogleVision> withJwt(String jwtPath,
-      [String scope = 'https://www.googleapis.com/auth/cloud-vision']) async {
-    GoogleVision yt = GoogleVision();
-    String _jsonCredentials = await rootBundle.loadString(jwtPath);
-
-    tokenGenerator =
-        JwtGenerator(jsonCredentials: _jsonCredentials, scope: scope, dio: dio);
-
-    await _confirmToken();
-
-    return yt;
-  }
-
-  static Future<void> _confirmToken() async {
-    if (tokenGenerator == null) {
-      throw Exception();
-    } else {
-      if (tokenExpiry.isBefore(DateTime.now())) {
-        final Token tokenData = await tokenGenerator!.generate();
-
-        _token = tokenData.accessToken;
-
-        tokenExpiry.add(Duration(seconds: tokenData.expiresIn));
-      }
-    }
-  }
-
   ///Run image detection and annotation for a batch of images.
-  Future<AnnotatedResponses> annotate({required AnnotationRequests requests}) {
-    return _rest.annotate(_authHeader, contentType, requests.toJson());
-  }
+  Future<void> annotate(String image) async {
+    assert(_accessToken != null);
+    assert(_rest != null);
 
-  ///draw a box on the supplied [Image] around detected object using [NormalizedVertex] values
-  static void drawAnnotationsNormalized(
-      Image image, List<NormalizedVertex> vertices,
-      {String color = 'red', int thickness = 3}) {
-    for (int index = 0; index < vertices.length - 1; index++) {
-      final NormalizedVertex vStart = vertices[index];
-
-      final NormalizedVertex vStop = vertices[index + 1];
-
-      image.drawLine(
-          (vStart.x * image.width).toInt(),
-          (vStart.y * image.height).toInt(),
-          (vStop.x * image.width).toInt(),
-          (vStop.y * image.height).toInt(),
-          RgbColor.name(color),
-          thickness: thickness);
-    }
-
-    image.drawLine(
-        (vertices.last.x * image.width).toInt(),
-        (vertices.last.y * image.height).toInt(),
-        (vertices.first.x * image.width).toInt(),
-        (vertices.first.y * image.height).toInt(),
-        RgbColor.name(color),
-        thickness: thickness);
-  }
-
-  ///draw a box on the supplied [Image] around the detected object using [Vertex] values
-  static void drawAnnotations(Image image, List<Vertex> vertices,
-      {String color = 'red', int thickness = 3}) {
-    for (int index = 0; index < vertices.length - 1; index++) {
-      final Vertex vStart = vertices[index];
-
-      final Vertex vStop = vertices[index + 1];
-
-      image.drawLine(vStart.x, vStart.y, vStop.x, vStop.y, RgbColor.name(color),
-          thickness: thickness);
-    }
-
-    image.drawLine(vertices.last.x, vertices.last.y, vertices.first.x,
-        vertices.first.y, RgbColor.name(color),
-        thickness: thickness);
-  }
-
-  ///draw [text] on the [Image] at the [x] and [y] position
-  static void drawText(Image image, int x, int y, String text) {
-    image.drawString(x, y, text);
+    VisionApi _vision = VisionApi(_rest!);
+    ImagesResource _api = _vision.images;
+    final AnnotateImageRequest request = AnnotateImageRequest(
+        image: Image(content: image),
+        features: <Feature>[
+          Feature(type: 'DOCUMENT_TEXT_DETECTION')
+        ]);
+    print(request.toJson());
+    BatchAnnotateImagesResponse response = await _api
+        .annotate(BatchAnnotateImagesRequest.fromJson(request.toJson()));
+    print(response.responses?.first.fullTextAnnotation);
   }
 }
 
+
 // class GoogleVision {
-//   GoogleVision() : assert(dotenv.env['GOOGLE_APPLICATION_CREDENTIALS'] != null);
+//   GoogleVision() : _rest = VisionClient(dio);
+//   final VisionClient _rest;
 //
-//   static AccessToken? _accessToken;
-//   static AutoRefreshingAuthClient? _rest;
+//   static final Dio dio = Dio();
+//   static final DateTime tokenExpiry = DateTime(2010, 0, 0);
+//   static const String accept = 'application/json';
+//   static const String contentType = 'application/json';
 //
-//   String? get _token => _accessToken?.data;
-//   String? get _tokenType => _accessToken?.type;
-//   DateTime? get tokenExpiry => _accessToken?.expiry;
+//   static TokenGenerator? tokenGenerator;
+//   static String? _token;
 //
-//   /// authenticate with jwt
-//   static Future<GoogleVision> withJwt(String jwtPath) async {
+//   String get _authHeader => 'Bearer $_token';
+//
+//   static Future<GoogleVision> withGenerator(TokenGenerator generator) async {
 //     final GoogleVision googleVision = GoogleVision();
-//     String _json = await rootBundle.loadString(jwtPath);
-//     ServiceAccountCredentials accountCredentials =
-//         ServiceAccountCredentials.fromJson(jsonDecode(_json));
-//     List<String> scopes = <String>[VisionApi.cloudVisionScope];
-//     AutoRefreshingAuthClient _client =
-//         await clientViaServiceAccount(accountCredentials, scopes);
 //
-//     _accessToken = _client.credentials.accessToken;
-//     _rest = _client;
+//     GoogleVision.tokenGenerator = generator;
+//
+//     await _confirmToken();
+//
 //     return googleVision;
 //   }
 //
-//   ///Run image detection and annotation for a batch of images.
-//   Future<void> annotate(String image) async {
-//     assert(_accessToken != null);
-//     assert(_rest != null);
+//   ///authenticated with JWT
+//   static Future<GoogleVision> withJwt(String jwtPath,
+//       [String scope = 'https://www.googleapis.com/auth/cloud-vision']) async {
+//     GoogleVision yt = GoogleVision();
+//     String _jsonCredentials = await rootBundle.loadString(jwtPath);
 //
-//     VisionApi _vision = VisionApi(_rest!);
-//     ImagesResource _api = _vision.images;
-//     BatchAnnotateImagesResponse response = await _api
-//         .annotate(BatchAnnotateImagesRequest.fromJson(<String, dynamic>{
-//       'requests': <Map<String, dynamic>>[
-//         <String, dynamic>{
-//           'image': <String, dynamic>{'content': image},
-//           'features': <Map<String, dynamic>>[
-//             <String, dynamic>{'type': 'DOCUMENT_TEXT_DETECTION'}
-//           ]
-//         }
-//       ]
-//     }));
+//     tokenGenerator =
+//         JwtGenerator(jsonCredentials: _jsonCredentials, scope: scope, dio: dio);
+//
+//     await _confirmToken();
+//
+//     return yt;
+//   }
+//
+//   static Future<void> _confirmToken() async {
+//     if (tokenGenerator == null) {
+//       throw Exception();
+//     } else {
+//       if (tokenExpiry.isBefore(DateTime.now())) {
+//         final Token tokenData = await tokenGenerator!.generate();
+//
+//         _token = tokenData.accessToken;
+//
+//         tokenExpiry.add(Duration(seconds: tokenData.expiresIn));
+//       }
+//     }
+//   }
+//
+//   ///Run image detection and annotation for a batch of images.
+//   Future<AnnotatedResponses> annotate({required AnnotationRequests requests}) {
+//     return _rest.annotate(_authHeader, contentType, requests.toJson());
+//   }
+//
+//   ///draw a box on the supplied [Image] around detected object using [NormalizedVertex] values
+//   static void drawAnnotationsNormalized(
+//       Image image, List<NormalizedVertex> vertices,
+//       {String color = 'red', int thickness = 3}) {
+//     for (int index = 0; index < vertices.length - 1; index++) {
+//       final NormalizedVertex vStart = vertices[index];
+//
+//       final NormalizedVertex vStop = vertices[index + 1];
+//
+//       image.drawLine(
+//           (vStart.x * image.width).toInt(),
+//           (vStart.y * image.height).toInt(),
+//           (vStop.x * image.width).toInt(),
+//           (vStop.y * image.height).toInt(),
+//           RgbColor.name(color),
+//           thickness: thickness);
+//     }
+//
+//     image.drawLine(
+//         (vertices.last.x * image.width).toInt(),
+//         (vertices.last.y * image.height).toInt(),
+//         (vertices.first.x * image.width).toInt(),
+//         (vertices.first.y * image.height).toInt(),
+//         RgbColor.name(color),
+//         thickness: thickness);
+//   }
+//
+//   ///draw a box on the supplied [Image] around the detected object using [Vertex] values
+//   static void drawAnnotations(Image image, List<Vertex> vertices,
+//       {String color = 'red', int thickness = 3}) {
+//     for (int index = 0; index < vertices.length - 1; index++) {
+//       final Vertex vStart = vertices[index];
+//
+//       final Vertex vStop = vertices[index + 1];
+//
+//       image.drawLine(vStart.x, vStart.y, vStop.x, vStop.y, RgbColor.name(color),
+//           thickness: thickness);
+//     }
+//
+//     image.drawLine(vertices.last.x, vertices.last.y, vertices.first.x,
+//         vertices.first.y, RgbColor.name(color),
+//         thickness: thickness);
+//   }
+//
+//   ///draw [text] on the [Image] at the [x] and [y] position
+//   static void drawText(Image image, int x, int y, String text) {
+//     image.drawString(x, y, text);
 //   }
 // }
--- lib/utils/text_recognizer.dart
+++ lib/utils/text_recognizer.dart
@@ -1,16 +1,11 @@
-import 'dart:io';
+
+import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../google_vision/google_vision.dart';
-import '../google_vision/models/annotated_responses.dart';
-import '../google_vision/models/annotation_request.dart';
-import '../google_vision/models/annotation_requests.dart';
-import '../google_vision/models/feature.dart';
-import '../google_vision/models/image.dart';
 
 class TextRecognizer {
   TextRecognizer()
@@ -20,17 +15,7 @@ class TextRecognizer {
     final GoogleVision googleVision = await GoogleVision.withJwt(
         dotenv.env['GOOGLE_APPLICATION_CREDENTIALS']!);
     final Uint8List encodedBytes = await file.readAsBytes();
-    final Image image = Image(encodedBytes);
-    final Image cropped = image.copyCrop(70, 30, 640, 480);
-    final AnnotationRequests requests = AnnotationRequests(requests: <AnnotationRequest>[
-      AnnotationRequest(image: cropped, features: <Feature>[
-        Feature(maxResults: 100, type: 'TEXT_DETECTION'),
-      ])
-    ]);
-
-    final AnnotatedResponses annotatedResponses =
-        await googleVision.annotate(requests: requests);
-
-    print(annotatedResponses);
+    String base64Image = base64Encode(encodedBytes);
+    final response = await googleVision.annotate(base64Image);
   }
 }
