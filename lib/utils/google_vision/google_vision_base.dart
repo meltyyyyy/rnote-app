df6da7b02524807489a8b1b6d6dd996e5c66c2f5
takeru.abe <takeru.abe@gaiax.com>
2022-07-13 20:53:39 +0900

    add drawAnnotations()

--- lib/utils/google_vision/google_vision_base.dart
+++ lib/utils/google_vision/google_vision_base.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 
+import 'package:color/color.dart';
 import 'package:flutter/services.dart';
 import 'package:googleapis/vision/v1.dart';
 import 'package:googleapis_auth/auth_io.dart';
-import 'package:rnote_app/utils/google_vision/vision_image.dart';
+import 'vision_image.dart';
 
 class GoogleVision {
   GoogleVision();
@@ -45,5 +46,27 @@ class GoogleVision {
   ///draw [text] on the [Image] at the [x] and [y] position
   static void drawText(VisionImage visionImage, int x, int y, String text) {
     visionImage.drawString(x, y, text);
+  }
+
+  ///draw a box on the supplied [Image] around the detected object using [Vertex] values
+  static void drawAnnotations(VisionImage visionImage, List<Vertex> vertices,
+      {String color = 'red', int thickness = 3}) {
+    for (int index = 0; index < vertices.length - 1; index++) {
+      final Vertex vStart = vertices[index];
+
+      final Vertex vStop = vertices[index + 1];
+
+      if(vStart.x != null && vStart.y != null && vStop.x != null && vStop.y != null){
+        visionImage.drawLine(vStart.x!, vStart.y!, vStop.x!, vStop.y!, RgbColor.name(color),
+            thickness: thickness);
+      }
+    }
+
+
+    if(vertices.last.x != null && vertices.last.y != null && vertices.first.x != null && vertices.first.y != null){
+      visionImage.drawLine(vertices.last.x!, vertices.last.y!, vertices.first.x!,
+          vertices.first.y!, RgbColor.name(color),
+          thickness: thickness);
+    }
   }
 }
