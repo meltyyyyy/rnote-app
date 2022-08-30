9ce193fb4160704842d4375d2d3e518e09c11703
takeru.abe <takeru.abe@gaiax.com>
2022-08-30 23:09:08 +0900

    refactor

--- lib/controllers/firebase/auth_controller.dart
+++ lib/controllers/firebase/auth_controller.dart
@@ -1,7 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../models/user.dart';
+import '../../providers/firebase/auth_provider.dart';
 
-class AuthController extends StateNotifier<User?> {
-  AuthController(super.state);
+class AuthController {
+  AuthController(this._ref) : super();
+
+  final Ref _ref;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<bool> signIn(String email, String password) async {
+    final UserCredential credential = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return credential.user != null;
+  }
+
+  Future<bool> signUp(String email, String password) async {
+    final UserCredential credential = await _auth
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return credential.user != null;
+  }
+
+  void signOut() {
+    _auth.signOut();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -7,7 +8,10 @@ import 'pages/launcher.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Future.wait<void>(<Future<dynamic>>[dotenv.load(fileName: '.env')]);
+  await Future.wait<void>(<Future<dynamic>>[
+    Firebase.initializeApp(),
+    dotenv.load(fileName: '.env')
+  ]);
   runApp(const ProviderScope(child: RnoteApp()));
 }
 
